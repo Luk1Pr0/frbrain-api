4794fba83f166040dbce7c5ae3a372193043b1c0
Luk1Pr0 <lpytel16@gmail.com>
2020-08-28 14:55:38 +0100

    Moved API to backend and ensured that empty details cannot be registered into the database

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "92f5c0602a32478584e32c294357ac38"
+});
+
+const handleApiCall = (req, resp) => {
+	app.models
+		.predict("c0c0ac362b03416da06ab3fa36fb58e3", req.body.input)
+		.then(data => {
+			resp.json(data);
+		})
+		.catch(err => resp.status(400).json("Unable to work with the API"))
+}
+
 const handleImage = (req, resp, db) => {
 	const { id } = req.body; 
 	db("users").where("id", "=", id)
@@ -10,5 +25,6 @@ const handleImage = (req, resp, db) => {
 }
 
 module.exports = {
-	handleImage: handleImage
+	handleImage,
+	handleApiCall 
 }
--- server.js
+++ server.js
@@ -24,11 +24,12 @@ db.select("*").from("users").then(data => {})
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get("/", (req, res) => {res.send(database.users) })
+app.get("/", (req, res) => {res.send(db.users) })
 app.post("/signin", (req, resp) => {signin.handleSignin(req, resp, db, bcrypt)});
 app.post("/register", (req, resp) => {register.handleRegister(req, resp, db, bcrypt)});
 app.get("/profile/:id", (req, resp) => {profile.handleProfileGet(req, resp, db)});
 app.put("/image", (req, resp) => {image.handleImage(req, resp, db)})
+app.post("/imageurl", (req, resp) => {image.handleApiCall(req, resp)})
 
 app.listen(2000, () => {
 	console.log("App is running on port 2000");
